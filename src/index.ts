a0423301f2e5006056cf987871d316d056b3cd7c
BlvckKryptonite <mumathedeveloper@gmail.com>
2026-06-17 09:11:01 -0500

    Adding mock Express data.

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,26 @@
-import express from 'express'
+import express, { type Express } from 'express'
 
 const PORT = 8000
 const app = express()
 
-app.listen(PORT, () => {
-    console.log("Listening on port: ", PORT)
+type Pet = {
+  name: string,
+  species: string,
+  adopted: boolean,
+  age: number
+}
+
+const pet: Pet = {
+  name: "Cicero",
+  species: "Doberman",
+  adopted: false,
+  age: 4
+}
+
+app.get('/', (req, res)=> {
+  res.json(pet)
+})
+
+app.listen(PORT, ()=>{
+  console.log("Listening on port: ", PORT)
 })
