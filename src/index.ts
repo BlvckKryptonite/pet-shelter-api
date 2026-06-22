a2630f0a6c73e589d28ae3f00f2f1e90e12fe218
BlvckKryptonite <mumathedeveloper@gmail.com>
2026-06-22 05:26:23 -0500

    Add typed pet filtering, ID lookup, and error responses

--- src/index.ts
+++ src/index.ts
@@ -3,20 +3,47 @@ import type {Express, Request, Response} from 'express'
 import cors from 'cors'
 
 import {pets} from './data/pets'
-import type { Pet } from './data/pets'
+import type {Pet} from './data/pets'
 
 const PORT = 8000
 const app:Express = express()
 
 app.use(cors())
 
-app.get('/', (req:Request, res:Response<Pet[]>):void=> {
-  res.json(pets)
+
+app.get('/', (
+  req:Request<{}, unknown, {}, {species?:string}>, 
+  res:Response<Pet[]>
+):void=> {
+  const {species} = req.query
+
+  let filteredPets:Pet[] = pets
+  
+  if (species){
+    filteredPets = filteredPets.filter((pet:Pet):boolean=>
+      pet.species.toLowerCase() === species.toLowerCase()
+    )
+  }
+  res.json(filteredPets)
 })
 
+app.get('/:id', (
+  req:Request<{id:string}>,
+  res:Response<Pet|{message:string}>):void=>{
+  const {id} = req.params
+  const pet:Pet|undefined = pets.find((pet:Pet):boolean => pet.id.toString() === id)
+  
+  if (pet){
+    res.json(pet)
+  } else {
+    res.status(404).json({message: "No pet with that ID"})
+  }
+  
+  
+})
 
 app.use((req:Request, res:Response<{message:string}>):void=>{
-  res.status(404).json({message: "No route found"})
+  res.status(404).json({message: "No endpoint found"})
 })
 
 app.listen(PORT, ():void =>{
